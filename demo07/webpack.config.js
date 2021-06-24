26e5cf83266cb136fd7f2323cf660b47a1baea31
zcyue <973294528@qq.com>
2021-06-24 22:13:20 +0800

    update

--- demo07/webpack.config.js
+++ demo07/webpack.config.js
@@ -3,8 +3,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
 
 module.exports = {
+	mode: 'development',
 	entry: {
 		index: `./src/index.js`
 	},
@@ -12,7 +14,6 @@ module.exports = {
 		path: path.join(__dirname, 'dist'),
 		filename: '[name]_[chunkhash:8].js'
 	},
-	mode: 'production',
 	module: {
 		rules: [
 			{
@@ -25,18 +26,32 @@ module.exports = {
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
+		new HtmlWebpackExternalsPlugin({
+			externals: [
+				{
+					module: 'react',
+					entry: 'https://11.url.cn/now/lib/16.2.0/react.min.js',
+					global: 'React',
+				},
+				{
+					module: 'react-dom',
+					entry: 'https://11.url.cn/now/lib/16.2.0/react-dom.min.js',
+					global: 'ReactDOM',
+				},
+			]
+		}),
 		new HtmlWebpackPlugin({
 			template: path.join(__dirname, `index.html`),
 			filename: `index.html`,
 			inject: true,
-			minify: {
-				html5: true,
-				collapseWhitespace: true,
-				preserveLineBreaks: false,
-				minifyCSS: true,
-				minifyJS: true,
-				removeComments: false
-			}
+			// minify: {
+			// 	html5: true,
+			// 	collapseWhitespace: true,
+			// 	preserveLineBreaks: false,
+			// 	minifyCSS: true,
+			// 	minifyJS: true,
+			// 	removeComments: false
+			// }
 		})
 	]
 };

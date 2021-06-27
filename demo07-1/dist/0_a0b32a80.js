781b0fbf6df7b6ea9653a74169e9f44cfaf94d0d
zcyue <973294528@qq.com>
2021-06-27 18:19:26 +0800

    update

--- demo07-1/dist/0_a0b32a80.js
+++ demo07-1/dist/0_a0b32a80.js
@@ -1,15 +1,14 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0], {
 
-/***/ "./src/text.js":
-/*!*********************!*\
-  !*** ./src/text.js ***!
-  \*********************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+  "./src/text.js":
+    (function (module, __webpack_exports__, __webpack_require__) {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function () {\n  return /*#__PURE__*/React.createElement(\"div\", null, \"\\u52A8\\u6001 import\");\n});\n\n//# sourceURL=webpack:///./src/text.js?");
+      "use strict";
+      __webpack_require__.r(__webpack_exports__);
+      __webpack_exports__["default"] = (function () {
+        return /*#__PURE__*/React.createElement("div", null, "\u52A8\u6001 import");
+      });
 
-/***/ })
+    })
 
 }]);
--- demo07-1/dist/1_6fe4fbc0.js
+++ demo07-1/dist/1_6fe4fbc0.js
@@ -1,15 +1,13 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1], {
 
-/***/ "./src/word.js":
-/*!*********************!*\
-  !*** ./src/word.js ***!
-  \*********************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+  "./src/word.js":
+    (function (module, __webpack_exports__, __webpack_require__) {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function () {\n  return /*#__PURE__*/React.createElement(\"div\", null, \"\\u52A8\\u6001 2\");\n});\n\n//# sourceURL=webpack:///./src/word.js?");
-
-/***/ })
+      "use strict";
+      __webpack_require__.r(__webpack_exports__);
+      __webpack_exports__["default"] = (function () {
+        return /*#__PURE__*/React.createElement("div", null, "\u52A8\u6001 2");
+      });
+    })
 
 }]);
